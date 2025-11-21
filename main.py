9a4ee38f346cc63cee50d273e894a6e9b230a8c2
Anders Frisgaard <anders.frisgaard@bestseller.com>
2025-11-21 13:31:47 +0100

    example of narwhals annoying type linting

---
+++ main.py
@@ -0,0 +1,15 @@
+import narwhals as nw
+from narwhals.typing import IntoFrameT
+
+
+def main():
+    print("Hello from tryout!")
+
+    def some_func(df_in: IntoFrameT) -> IntoFrameT:
+        """Some random func"""
+        df = nw.from_native(df_in)
+        return df
+
+
+if __name__ == "__main__":
+    main()
